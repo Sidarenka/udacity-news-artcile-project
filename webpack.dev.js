64ed31fed613aa87368cc1f0403aada5d692dcab
Galina Sidarenka <galia.sidarenka@icloud.com>
2021-12-28 21:19:06 -0500

    added WorkboxPlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const WorkboxPlugin = require('workbox-webpack-plugin')
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -26,6 +28,7 @@ module.exports = {
         ]
     },
     plugins: [
+        // new WorkboxPlugin.GenerateSW(),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
@@ -38,6 +41,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+
     ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -20,9 +21,11 @@ module.exports = {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
             }, 
+
         ]
     },
     plugins: [
+        new WorkboxPlugin.GenerateSW(),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
